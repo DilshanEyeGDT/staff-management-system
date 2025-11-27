85fd7f105d1c3048b6cd3ec5ef6a5c85e56859a0
DilshanEyeGDT <tharaka.d@eyepax.com>
2025-11-27 10:10:20 +0530

    [NET] Upload a job schedule CSV

--- asp_net/StaffManagement/Program.cs
+++ asp_net/StaffManagement/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddScoped<ScheduleService>();      //register ScheduleService
 builder.Services.AddScoped<TaskService>();          //register TaskService
+builder.Services.AddScoped<ImportService>();        //register ImportJobsService
 
 //database connection
 builder.Services.AddDbContext<AppDbContext>(options =>
---
+++ asp_net/StaffManagement/Services/ImportService..cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StaffManagement.Persistence;
+using StaffManagement.Persistence.Entities;
+
+public class ImportService
+{
+    private readonly AppDbContext _db;
+
+    public ImportService(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<Guid> CreateScheduleImportJobAsync(IFormFile file, int? userId)
+    {
+        if (file == null || file.Length == 0)
+            throw new InvalidOperationException("File is empty.");
+
+        // Generate unique Job ID
+        var jobId = Guid.NewGuid();
+
+        var job = new ImportJob
+        {
+            JobId = jobId,
+            UserId = userId,
+            FileName = file.FileName,
+            Status = "Pending",
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow,
+            Result = null
+        };
+
+        _db.ImportJobs.Add(job);
+        await _db.SaveChangesAsync();
+
+        return jobId;
+    }
+}
