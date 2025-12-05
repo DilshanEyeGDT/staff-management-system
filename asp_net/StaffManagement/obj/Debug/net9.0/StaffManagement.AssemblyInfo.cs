1777d8b560e365898d2ac64dee05f72435fba4a5
DilshanEyeGDT <tharaka.d@eyepax.com>
2025-12-05 12:11:28 +0530

    [NET] Setup test environemnt

--- asp_net/StaffManagement/obj/Debug/net9.0/StaffManagement.AssemblyInfo.cs
+++ asp_net/StaffManagement/obj/Debug/net9.0/StaffManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StaffManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ade06ee23e20ac7c6925c7fc4c769368a8028bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab2ee87682c06c434353e2ed4bcbd766a3d2b88e")]
 [assembly: System.Reflection.AssemblyProductAttribute("StaffManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StaffManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
